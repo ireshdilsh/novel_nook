8e8bd81972949350cda8e5d02db05c89a37c8548
ireshdilsh <ireshdilshan46@gmail.com>
2024-09-28 18:38:40 +0530

    add java.sql module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.book.shop {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.mail;
-
+    requires java.sql;
 
     opens com.book.shop to javafx.fxml;
     exports com.book.shop;
